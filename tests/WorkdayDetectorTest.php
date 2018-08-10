d5aa1125b386f62cde0831a1a7194c1131a01b9e
Theodore R. Smith <theodore@phpexperts.pro>
2018-08-09 22:26:19 -0500

    Added the license and copyright headers.

--- tests/WorkdayDetectorTest.php
+++ tests/WorkdayDetectorTest.php
@@ -1,5 +1,16 @@
 <?php declare(strict_types=1);
 
+/**
+ * This file is part of the Workday Planner, a PHP Experts, Inc., Project.
+ *
+ * Copyright © 2018 PHP Experts, Inc.
+ * Author: Theodore R. Smith <theodore@phpexperts.pro>
+ *  GPG Fingerprint: 4BF8 2613 1C34 87AC D28F  2AD8 EB24 A91D D612 5690
+ *  https://www.phpexperts.pro/
+ *
+ * This file is licensed under the MIT License.
+ */
+
 namespace PHPExperts\WorkdayPlanner\Tests;
 
 use PHPExperts\WorkdayPlanner\WorkdayDetector;
